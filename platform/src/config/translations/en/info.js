fb1d96a681ce527f6e24aa8d7eb3b81c2493f43a
yunica <fmjg21@gmail.com>
2021-04-29 09:50:18 -0500

    update content

--- platform/src/config/translations/en/info.js
+++ platform/src/config/translations/en/info.js
@@ -5,25 +5,23 @@ export default {
 
 ## About
 
-The [“Guidance for Sustainable Natural Rubber”](http://www.cccmc.org.cn/docs/2018-02/20180224085322440051.pdf) (hereinafter referred to as the “Guidance”) released by the China Chamber of Commerce for Import and Export of Minerals and Chemicals (CCCMC) is applicable for improving the governance capacity of enterprises engaged in natural rubber investment, plantation and operation, to identify, prevent and manage economic, social and environmental risks encountered in the process of their operations. In order to guide the upstream and downstream enterprises in the natural rubber industry, as well as relevant government departments, regulatory agencies, financial institutions and other stakeholders to timely understand and obtain risk information, manage and control the risk, and jointly achieve responsible and sustainable natural rubber investment and operation, CCCMC aims to develop an interactive platform for early warning and analysis based on the risk framework proposed in the Guidance, as an online tool to assist in the implementation of the Guidance.
+The [“Guidance for Sustainable Natural Rubber”](http://www.cccmc.org.cn/docs/2018-02/20180224085322440051.pdf) (hereinafter referred to as the “Guidance”) released by the China Chamber of Commerce of Metals, Minerals & Chemicals Importers & Exporters (CCCMC) is intended to improve the governance capacity of enterprises engaged in the investment, farming, and processing of natural rubber to identify, prevent and manage economic, social and environmental risks. The guidance is designed to guide multiple stakeholders’ engagements in the natural rubber supply chain from upstream sustainable and responsible rubber cultivation to downstream rubber investments. CCCMC aims to develop an interactive platform for early warning and analysis based on the risk framework proposed in the Guidance, as an online tool to assist in the implementation of the Guidance. This online tool will guide the natural rubber industry’s upstream and downstream enterprises; relevant government departments; regulatory agencies; financial institutions; and other stakeholders in understanding and obtaining risk information in a timely manner, managing and controlling risk, and jointly achieving responsible and sustainable natural rubber investment and operations.
 <!--cccmc-diagram -->
 <img src="/assets/images/cccmc-diagram.png" width="100%"></img>
 
-_The diagram to showcase how we map rubber plantation in SE Asia with machine learning, rubber risk data sourcing, processing and serving and the platform development_
+_The diagrams showcase how we map rubber plantations in SE Asia with machine learning, rubber risk data sourcing, processing and serving, and the platform development_
 
-The platform was developed by CCCMC and Development Seed collaboratively.
+The platform was collaboratively developed by CCCMC and Development Seed.
 
 For more information including:
-- how rubber plantations were mapping with machine learning;
-- how rubber risk data layers were sourced, processed and served to the platform.
+- how rubber plantations were mapped using machine learning;
+- how rubber risk data layers were sourced, processed, and served to the platform.
 
 You can visit the platform [data info page](https://cccmc-sustainable-rubber.surge.sh/datainfo). The data processing scripts can be found on the Development Seed's [GitHub repo](https://github.com/developmentseed/cccmc-sustainable-rubber).
 
 ## Disclaimer
 
-The platform, processed dataset, and source codes are open-sourced under MIT Licence. For more information please see: https://github.com/developmentseed/cccmc-sustainable-rubber-pub/blob/master/LICENSE.
-We don't collect any user information and if you have any platform related feedback, please go to bottom left feedback box to write email to "rbc@cccmc.org.cn".
-All data layers were processed before Feb, 2021.
+The platform, processed dataset, and source codes are open-sourced under MIT Licence. For more information please see [https://github.com/developmentseed/cccmc-sustainable-rubber-pub/blob/master/LICENSE](https://github.com/developmentseed/cccmc-sustainable-rubber-pub/blob/master/LICENSE). We don't collect any user information and if you have any platform-related feedback, please go to the bottom left feedback box to write an email to "rbc@cccmc.org.cn". All data layers were processed before Feb 2021.
 
 `
 }
